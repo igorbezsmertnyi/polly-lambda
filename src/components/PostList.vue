8485a5456c6fe09476a6cd276459cdaf6addff0d
igorbezsmertnyi <bezsmertnyi.igor@gmail.com>
2018-08-31 23:40:24 +0200

    add dialog

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -9,7 +9,7 @@
     <el-table v-if="posts.length" :data="posts" style="width: 100%" height="450">
       <el-table-column label="Text">
         <template slot-scope="scope">
-          <p>{{ scope.row.text }}</p>
+          <p @click="toggleDialog(scope.row)">{{ scope.row.text }}</p>
         </template>
       </el-table-column>
       <el-table-column label="Listen" width="320">
@@ -20,6 +20,29 @@
         </template>
       </el-table-column>
     </el-table>
+
+
+    <el-dialog class="dialog" :visible.sync="outerVisible">
+      <el-dialog
+          width="30%"
+          title="Inner Dialog"
+          append-to-body>
+      </el-dialog>
+
+      <div class="dialog__content">
+        <audio controls>
+          <source :src="dialogAudio" type="audio/ogg">
+        </audio>
+
+        <p class="h2">
+          {{dialogText}}
+        </p>
+      </div>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="outerVisible = false" round>Close</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -28,6 +51,11 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class PostList extends Vue {
+  outerVisible: boolean = false
+  dialogVisible: boolean = false
+  dialogText: string = ''
+  dialogAudio: string = ''
+
   beforeMount() {
     this.$store.dispatch('getPosts')
   }
@@ -35,5 +63,26 @@ export default class PostList extends Vue {
   get posts() {
     return this.$store.getters.posts
   }
+
+  toggleDialog(row: any) {
+    this.outerVisible = !this.outerVisible
+    this.dialogText = row.text
+    this.dialogAudio = row.url
+  }
 }
 </script>
+
+<style lang="stylus">
+.dialog
+  &__content
+    display flex
+    flex-direction column
+    align-items center
+    
+    audio
+      width 100%
+
+    p
+      width 100%
+      margin-top 30px
+</style>
